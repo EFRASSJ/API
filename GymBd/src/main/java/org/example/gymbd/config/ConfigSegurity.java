43adc46e8f35193a7818721840d49a10ec3495ad
Nadia05Sa <20233tn237@utez.edu.mx>
2024-12-10 23:06:56 -0600

    cambios

---
+++ GymBd/src/main/java/org/example/gymbd/config/ConfigSegurity.java
@@ -0,0 +1,12 @@
+package org.example.gymbd.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigSegurity {
+
+
+}
+
